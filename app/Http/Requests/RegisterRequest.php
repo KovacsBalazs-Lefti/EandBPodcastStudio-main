2a5748194eb669fc8e9606dd5668dba4f39d5376
kovacbalazs-Lefti <kovacs.balazs@petrik.hu>
2024-04-27 14:56:57 +0200

    hibaüzenetek kialakítása - teszteléshez- kész

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -47,4 +47,35 @@ class RegisterRequest extends FormRequest
 
         ];
     }
+    public function messages(): array
+    {
+        return[
+            "nev.required" => "A név mező kitöltése kötelező",
+            "email.required" => "Az emailcím kötöltése kötelező",
+            "email.email" => "A név mező kitöltése kötelező",
+            "email.unique" => "Ez az emailcím már foglalt",
+            "jelszo.required" => "A jelszó mező kitöltése kötelező",
+            "jelszo.min" => "A jelszónak legalább 8 karakternek kell lennie",
+            "telefonszam.required" => "A telefonszám mező kitöltése kötelező",
+            "szemelyi_szam.required" => "A személyi szám mező kitöltése kötelező",
+            "szemelyi_szam.min" => "A személyi szám mezőnek legalább 8 karakternek kell lennie",
+            "szuletesi_datum.required" => "A születési dátum mező kitöltése kötelező",
+            "szuletesi_datum.date_format" => "A születési dátum formátuma nem megfelelő. (Pl.: YYYY-MM--DD)",
+            "szuletesi_datum.before" => "A felhasználónak legalább 18 évesnek kell lennie",
+            "cegnev" =>"A cégnév mező kitöltése kötelező,ha a Cég mező ki van jelölve",
+            "ceg_tipus_if" => "A cég típusa mező kitöltése kötelező,ha a Cég mező ki van jelölve",
+            "ado_szam_if" => "Az adószám mező kitöltése kötelező,ha a Cég mező ki van jelölve",
+            "bankszamlaszam_if" => "A bankszámlaszám megadása kitöltése kötelező,ha a Cég mező ki van jelölve",
+            "orszag" =>"Az ország mező kitöltése kötelező",
+            "iranyitoszam" => "Az irányítószám mező kitöltése kötelező",
+            "varos" => "A város mező kitöltése kötelező",
+            "utca" => "Az utca mező kitöltése kötelező",
+            "utca_jellege" => "Az utca jellege mező kitöltése kötelező",
+            "hazszam" => "A házszám mező kitöltése kötelező",
+            "epulet" => "Az épület mező kitöltése kötelező",
+            "lepcsohaz" => "A lépcsőház mező kitöltése kötelező",
+            "emelet" => "Az emelet mező kitöltése kötelező",
+            "ajto" => "Az ajtó mező kitöltése kötelező",
+        ];
+    }
 }
